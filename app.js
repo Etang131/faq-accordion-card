7c92bfb503a8d123af2e2d5d49183e9e2fe46385
Erik Tangvik <erikjt@outlook.com>
2020-12-13 16:01:58 -0600

    notes of what needs to be done

---
+++ app.js
@@ -0,0 +1,12 @@
+const accordion = document.getElementsByClassName("faqQuestion");
+
+//Make .faqQuestion bold = 700
+//Make .faqAnswer
+//   height: 100px;
+//   margin-top: 10px;
+
+//Make hr
+// margin: 45px 5% 0.75rem 5%;
+
+//Make faqQuestion::after
+// roate 180 deg
