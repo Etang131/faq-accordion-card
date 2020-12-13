d768b5887d68cf7fc0f189e0dfe7122524009a31
Erik Tangvik <erikjt@outlook.com>
2020-12-13 16:42:49 -0600

    I think I'm on the right track

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-const accordion = document.getElementsByClassName("faqQuestion");
+const questions = document.querySelectorAll(".faqQuestion");
+
+const faqQuestion = document.querySelectorAll(".faqQuestion");
+const faqAnswer = document.querySelectorAll(".faqAnswer");
+const hr = document.querySelectorAll("hr");
+
+questions.forEach((question) => {
+  question.addEventListener("click", (showAnswer) => {
+    if (question.classList.contains("active")) {
+      console.log("Cows are in the park");
+      faqQuestion.classList.remove("active");
+      faqAnswer.classList.remove("active");
+      // faqQuestion::after.classList.remove("active"); not sure how to type this one
+      hr.classList.remove("active");
+    } else {
+      console.log("cows are not in the park");
+      faqQuestion.classList.add("active");
+      faqAnswer.classList.add("active");
+      // faqQuestion::after.classList.add("active"); not sure how to type this one
+      hr.classList.add("active");
+    }
+  });
+});
 
 //Make .faqQuestion bold = 700
 //Make .faqAnswer
