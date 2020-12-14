40149874af791f443bcdb908f26b5797727395c2
Erik Tangvik <erikjt@outlook.com>
2020-12-13 21:29:27 -0600

    arrow now rotates

--- app.js
+++ app.js
@@ -4,24 +4,23 @@ const faqQuestion = document.querySelectorAll(".faqQuestion");
 const faqAnswer = document.querySelectorAll(".faqAnswer");
 const hr = document.querySelectorAll("hr");
 
-//Can't get the forEach statement to get the right index
-questions.forEach((question) => {
+questions.forEach((question, i) => {
   question.addEventListener("click", (showAnswer) => {
     if (question.classList.contains("active")) {
-      console.log("Cows are in the park");
-      faqQuestion.classList.remove("active");
-      faqAnswer.classList.remove("active");
-      // faqQuestion::after.classList.remove("active"); not sure how to type this one
-      hr.classList.remove("active");
+      faqQuestion[i].classList.remove("active");
+      faqAnswer[i].classList.remove("active");
+      hr[i].classList.remove("active");
     } else {
-      console.log("cows are not in the park");
-      faqQuestion(showAnswer).classList.add("active");
-      faqAnswer[0].classList.add("active");
-      // faqQuestion::after.classList.add("active"); not sure how to type this one
-      hr.classList.add("active");
+      faqQuestion[i].classList.add("active");
+      faqAnswer[i].classList.add("active");
+      hr[i].classList.add("active");
     }
   });
 });
+
+// Very Important note on Pesuedo elements check css as well
+
+// https://stackoverflow.com/questions/21032481/change-the-style-of-before-and-after-pseudo-elements
 
 //Make .faqQuestion bold = 700
 //Make .faqAnswer
